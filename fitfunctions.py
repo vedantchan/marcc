c76d67196b95d2a5188364625ab730bbb1ece948
Vedant Chandra <me@vedantchandra.com>
2019-08-02 19:50:52 -0400

    data driven generator

--- fitfunctions.py
+++ fitfunctions.py
@@ -1,11 +1,7 @@
-def teffpred(spectrum):
-    fitter = lmfit.Model(gen_sampler)
-    params = lmfit.Parameters()
-    params.add('t',min=5000,max=80000,value=13000)
-    params.add('l',min=6.5,max=9.5,value=8.)
-    params.add('scale',min=0.1,max=5,value=1)
-    params.add('trans',min=-5,max=5,value=0)
-    params['l'].set(value = 8.,vary=False)
-    params['trans'].set(value = 0,vary=True)
-    result = fitter.fit(spectra[i][mask],params,wl=lamgrid[mask],method='powell',nan_policy='omit')
+def MCMC_fit(spectrum,model,params,grid,ncores,lines):
+    import emcee
+    import lmfit
+
+    result = model.fit(spectrum[lines],params,wl=grid,method="emcee",nan_policy='omit',\
+                             calc_covar=True, fit_kws={'nwalkers':500,'steps':25})
     return result
